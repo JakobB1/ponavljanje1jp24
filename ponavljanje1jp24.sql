baacba4c3c3007591f71c50ac782116b01c05eb3
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-28 15:03:26 +0100

    createTables

--- ponavljanje1jp24.sql
+++ ponavljanje1jp24.sql
@@ -2,3 +2,43 @@ drop database if exists ponavljanje1jp24;
 create database ponavljanje1jp24;
 use ponavljanje1jp24;
 
+create table pcshop(
+  sifra int not null primary key auto_increment,
+  naziv varchar(30) not null,
+  oib varchar(40) not null,
+  djelatnik int,
+  vlasnik int
+);
+
+create table djelatnik(
+  sifra int not null primary key auto_increment,
+  ime varchar(50) not null,
+  prezime varchar(50) not null,
+  pcshop int
+);
+
+create table racun(
+  sifra int not null primary key auto_increment,
+  datumpocetka datetime not null,
+  brojracuna varchar(100) not null,
+  korisnik int
+);
+
+create table proizvod(
+  sifra int not null primary key auto_increment,
+  naziv varchar(30) not null,
+  cijena decimal(12,8) not null,
+  garancija datetime not null
+);
+
+create table stavka(
+  sifra int not null primary key auto_increment,
+  proizvod int not null,
+  stavka int not null
+);
+
+create table korisnik(
+  sifra int not null primary key auto_increment,
+  ime varchar(30) not null,
+  prezime varchar(30) not null
+);
